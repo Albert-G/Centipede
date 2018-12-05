3be92853ddec830b1f15dcfcb8d83ad0274aee04
Albert-G <gao_xin@live.com>
2018-12-05 16:06:47 +0800

    Added interactive mode and batch mode

--- Centipede.Console/Program.cs
+++ Centipede.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -11,12 +12,54 @@ namespace Centipede
 
         static void Main(string[] args)
         {
+            if (args.Length <= 0)
+            {
+                PrintUsage();
+                return;
+            }
+
             var path = Path.GetFullPath(args.Length > 0 ? args[0].Replace("\"", "") : "./ ");
 
-            Console.WriteLine($"Processing project files under {path} ... ");
+            Console.WriteLine($"Processing files under {path} ... ");
             _centipedeInstance = new Centipede(new ConsoleLogger(), path);
 
             Console.WriteLine("Done");
+
+            if (args.Length == 1)
+            {
+                StartInteractiveMode();
+            }
+            else
+            {
+                StartBatchMode(args.Skip(1));
+            }
+        }
+
+        private static void StartBatchMode(IEnumerable<string> fileNames)
+        {
+            var hostSolutions = new List<VsProjectFile>();
+            foreach (var fileName in fileNames)
+            {
+                hostSolutions.AddRange(_centipedeInstance.GetHostSolutions(fileName));
+            }
+            hostSolutions = hostSolutions.Distinct().ToList();
+
+            if (hostSolutions.Count() <= 0)
+            {
+                Console.WriteLine($"No result.");
+            }
+            else
+            {
+                Console.WriteLine("Solutions: ");
+                foreach (var solution in hostSolutions)
+                {
+                    Console.WriteLine(solution.Path);
+                }
+            }
+        }
+
+        private static void StartInteractiveMode()
+        {
             ReadLine.AutoCompletionHandler = new AutoCompletionHandler();
 
             while (true)
@@ -42,6 +85,16 @@ namespace Centipede
                     }
                 }
             }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine();
+            Console.WriteLine("centipede <folder path> [file1 full path] [file2 full path] ...");
+            Console.WriteLine();
+            Console.WriteLine("If no file given, Centipede will enable interactive mode, enter part of the file and use tab key to autocomplte the full path and press enter to get the result.");
+            Console.WriteLine("Type 'exit' to quit Centipede.");
         }
 
         class AutoCompletionHandler : IAutoCompleteHandler
